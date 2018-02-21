ac5fc5e6c7242831381baa87ba86b2d6e708fec6
Alexander <alexandermarenco4@gmail.com>
2018-02-21 10:47:12 -0600

    Configurando api last.fm - creando metodo obtener artistas(FETCH)

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+  appName: 'vuemusic',
+  apiKey: 'e7f9ec7bf075e4a6d2352f0a55b283a4',
+  secret: '49ba1d5d323874f7172df506023dff88',
+  registeredTo: 'alxd505'
+}
+
+
+// Application name --	vuemusic
+// API key --	e7f9ec7bf075e4a6d2352f0a55b283a4
+// Shared secret --	49ba1d5d323874f7172df506023dff88
+// Registered to --	alxd505
