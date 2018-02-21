f196ca223f3c064c5f769b73e1f7dd2b8b137b2b
Alexander <alexandermarenco4@gmail.com>
2018-02-21 11:22:52 -0600

    Estructurando componenetes - creando Artist.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
     img(src='./assets/logo.png')
     h1 VueMusic
     ul
-      li(v-for="artist in artists") {{artist.name}}
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 
 </template>
 
 <script>
+  import Artist from './components/Artist.vue'
   import getArtists from './api'
 
   export default {
@@ -16,6 +17,9 @@
       return {
         artists: []
       }
+    },
+    components:{
+      Artist: Artist
     },
     mounted: function(){
       const self = this
